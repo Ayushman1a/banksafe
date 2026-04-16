d800dbe10d11871a8f4bf767180b6f941304fd7c
Ayushman1a <dummy@gmail.com>
2026-04-16 14:32:16 +0000

    day12

--- server/src/main/java/com/edutech/progressive/BankSafeApplication.java
+++ server/src/main/java/com/edutech/progressive/BankSafeApplication.java
@@ -10,7 +10,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = "com.edutech.progressive.repository")
 public class BankSafeApplication {
     public static void main(String[] args) {
-        // Keep the same message you already had (your Day 4 test passed with this)
         System.out.println("Welcome to BankSafe Project!");
         SpringApplication.run(BankSafeApplication.class, args);
     }
